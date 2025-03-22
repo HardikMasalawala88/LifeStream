87884ec4ef46b47f7601d97c85489f2fe6823c7a
sujal modi <sujalmodi700@gmail.com>
2025-03-22 16:40:50 +0530

    HomeSide

--- LifeStream/Migrations/20250320123523_ls.Designer.cs
+++ LifeStream/Migrations/20250320123523_ls.Designer.cs
@@ -4,6 +4,7 @@ using LifeStream.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LifeStream.Migrations
 {
     [DbContext(typeof(LifeStreamdDBContext))]
-    partial class LifeStreamdDBContextModelSnapshot : ModelSnapshot
+    [Migration("20250320123523_ls")]
+    partial class ls
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
